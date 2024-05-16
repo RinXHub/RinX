856451fd0f2ed8064582ddc34878ad040b6f702b
RinXHub <167017943+RinXHub@users.noreply.github.com>
2024-05-16 23:43:19 +0700

    Update new.lua

--- new.lua
+++ new.lua
@@ -1,2 +1,2 @@
-setclipboard("https://discord.com/invite/bK7wPrRERW")
+setclipboard("https://discord.com/invite/SKxx9MbkUe")
 game.Players.LocalPlayer:Kick("New Update Join Discord For Script | có update mới vào discord lấy | https://discord.com/invite/bK7wPrRERW")
